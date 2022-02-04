a4d650be37b52ce50cd97e2888af2b9d9e987855
Mohammad <mo@mohammad.dev>
2022-02-03 22:11:13 -0500

    temp change

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CssBaseline, Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+    </div>
+  );
+};
+
+export default App;
