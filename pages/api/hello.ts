0018a0b147944120398e784294120e9750968f59
Mohammad <mo@mohammad.dev>
2022-02-10 22:58:00 -0500

    check database connectivity

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { portfolio, PrismaClient } from "@prisma/client";
 
-type Data = {
-  name: string
-}
+const prisma = new PrismaClient()
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<portfolio[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const portfolios = await prisma.portfolio.findMany();
+  console.log(portfolios)
+  await res.status(200).json(portfolios);
 }
